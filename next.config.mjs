43133d22f1516d33c993c7d5ced7be439275cdb8
VMK-004 <vmkrishna2002@gmail.com>
2025-07-01 10:08:41 -0400

    Deploying the app

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,31 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    serverComponentsHmrCache: false,
+  },
+
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "xnaqekboepqotwvbpfdv.supabase.co",
+      },
+    ],
+  },
+
+  async headers() {
+    return [
+      {
+        source: "/embed",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: "frame-src 'self' https://vehiql-waitlistform.created.app",
+          },
+        ],
+      },
+    ];
+  },
+};
 
 export default nextConfig;
